8e63b9a47c0d65f3e808f2841ec61293f84637ae
Negrebetskiy <kmiziz.com@gmail.com>
2022-03-18 15:08:14 +0300

    changed version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="ratelib",
-    version="0.1.0",
+    version="0.1.1",
     description="Python interface to REACLIB databases",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
